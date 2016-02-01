d7b10f5254a763d8f9f690f2d3a4623b91e2aeaa
Isis T. Baulig <grindcode@gmail.com>
2016-02-01 08:57:24 +0100

    add type checking

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = function (res, val, i) {
+  if (typeof res !== 'string' && !Array.isArray(res))
+    throw new TypeError('First argument invalid. Expected Array or String.')
+  if (typeof i !== 'undefined' && typeof i !== 'number')
+    throw new TypeError('Third argument invalid. Expected Number.')
   return res.slice(0, i || 0).concat(val).concat(res.slice(i || 0))
 }
